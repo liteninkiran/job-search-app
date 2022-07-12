8318c83d37c3286dbb0aee7214a84b8b38c5cc9b
Kiran Anand <liteninkiran@hotmail.com>
2022-07-12 23:43:41 +0100

    Updating subscription

--- resources/client/src/app/components/job-type-create/job-type-create.component.ts
+++ resources/client/src/app/components/job-type-create/job-type-create.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { JobType } from '../job-types/job-type';
 import { JobTypeService } from '../job-types/job-type.service';
 
@@ -9,24 +10,29 @@ import { JobTypeService } from '../job-types/job-type.service';
     templateUrl: './job-type-create.component.html',
     styleUrls: ['./job-type-create.component.css'],
 })
-export class JobTypeCreateComponent implements OnInit {
+export class JobTypeCreateComponent implements OnInit, OnDestroy {
 
     public jobType = new JobType();
     public errors: any;
+    public subCreate: Subscription = new Subscription;
 
     constructor(
         private router: Router,
         private jobTypeService: JobTypeService,
     ) { }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+        this.subCreate.unsubscribe();
     }
 
     public addJobType() {
-        this.jobTypeService.addJobType(this.jobType).subscribe(res => {
-            this.router.navigateByUrl('/job_types');
-        }, (error: HttpErrorResponse) => {
-            this.errors = error.error.errors;
+        this.subCreate = this.jobTypeService.addJobType(this.jobType).subscribe({
+            next: (data) => this.router.navigateByUrl('/job_types'),
+            error: (error: HttpErrorResponse) => this.errors = error.error.errors,
+            complete: () => console.info('complete') 
         });
     }
 }
